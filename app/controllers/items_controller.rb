cfeaf82b16ba6348b60d923b5627eef2dada933e
aleksandrshliakhovoi <aleksandrshliakhovoi@gmail.com>
2021-03-11 21:00:47 +0200

    Add params permit settings

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,24 @@
 class ItemsController < ApplicationController
   layout false #doesnt require a view
   #seacrh all goods in db
+
+  skip_before_action :verify_authenticity_token
+
   def index
     @items = Item.all
     render body: @items.map { |i| "#{i.name}:#{i.price}"}
   end
 
   def create
-    Item.create(items_params)
-    render body: 'create'
+    item = Item.create(items_params)
+
+    if item.persisted?
+      render json: item.name, status: :created
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+
+    
   end
 
 
