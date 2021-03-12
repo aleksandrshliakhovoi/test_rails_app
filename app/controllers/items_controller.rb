d9e8e0f3bac84d336b5d6fe2bbfdabba09e36ca5
aleksandrshliakhovoi <aleksandrshliakhovoi@gmail.com>
2021-03-12 03:09:38 +0200

    Add forms

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,16 @@
 class ItemsController < ApplicationController
-  layout false #doesnt require a view
-  #seacrh all goods in db
+  layout false # doesnt require a view
+  # seacrh all goods in db
 
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token # skip before endpoints
 
+  # show all items in db
   def index
     @items = Item.all
     render body: @items.map { |i| "#{i.name}:#{i.price}"}
   end
 
+  # create one object
   def create
     item = Item.create(items_params)
 
@@ -20,6 +22,18 @@ class ItemsController < ApplicationController
 
     
   end
+
+  def new; end
+
+  # show current id
+  def show; end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+
 
 
   private
