b3200620c41f9fdad8bc1bcc8079f15afea60d06
aleksandrshliakhovoi <aleksandrshliakhovoi@gmail.com>
2021-04-17 22:16:36 +0300

    Add order model

--- db/migrate/20210404002406_create_orders.rb
+++ db/migrate/20210404002406_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
-
+      t.integer :user_id # orders may have many users_id
       t.timestamps
     end
   end
