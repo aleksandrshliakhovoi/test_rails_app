15d7b387fa64b740ff3b7d68d02205324cf36851
aleksandrshliakhovoi <aleksandrshliakhovoi@gmail.com>
2021-04-23 02:43:30 +0300

    Add custom helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def urls_to_content(str)
+
+    str.gsub!(%r{https://[^\s<]+}) do |url|
+      if url[/(?:png|jpe?g|jpg|gif|svg)$/]
+        "<img src='#{url}' width='100'; />"
+      else
+        "<a href='#{url}'>link to info</a>"
+      end
+    end
+    str.html_safe
+  end
+  
+
 end
